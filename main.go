8c0863ac5a562d6926c46a7596b1e1ad502f1013
Xiangrui2019 <xiangruikong2019@outlook.com>
2019-06-20 13:45:30 +0800

    Terst

--- main.go
+++ main.go
@@ -1,33 +1,19 @@
 package main
 
 import (
-	"context"
 	"github.com/gin-gonic/gin"
-	"github.com/xiangrui2019/redis"
+	"github.com/garyburd/redigo/redis"
 	"net/http"
 )
 
 const IPMAXConnection = 2
 const Secound = 1
 
-var redisconn redis.Client
-var ctx context.Context
-
+var client redis.Conn
 
 
 func main() {
 	app := gin.Default()
-	ctx = context.Background()
-	redisconn = redis.New(redis.Options{
-		Address: "localhost:6379",
-		PoolSize: 10,
-	})
-
-	err := redisconn.Ping(ctx)
-
-	if err != nil {
-		panic(err)
-	}
 
 	app.GET("/", func(context *gin.Context) {
 		ip := context.ClientIP()
